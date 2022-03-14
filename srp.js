d7a12b25163c62727c4517915f772839cdd55500
Burykin <wowa.bur2004@gmail.com>
2022-03-14 12:26:31 +0700

    3.3

--- srp.js
+++ srp.js
@@ -1,5 +1,23 @@
 $(document).ready(function() {
+   $("#descr1").dialog({width:200,height:300,autoOpen:false});
+    $("#descr2").dialog({modal:true,autoOpen:false});
+   $("#descr3").dialog({autoOpen:false,buttons:{
+      OK:function(){
+         $(this).dialog("close");},
+      Отмена:function(){
+         $(this).dialog("close");}}
+   });
 
-   $("#tabs").tabs();
+   $("#but3").click(function(){
+      $("#descr3").dialog("open"); 
 
+   });
+      $("#but2").click(function(){
+      $("#descr2").dialog("open"); 
+      
+   });
+         $("#but1").click(function(){
+      $("#descr1").dialog("open"); 
+      
+   });
 });
