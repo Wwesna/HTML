630155c673d7c514566053fc2b3a0b9ea6cbbae0
Burykin <wowa.bur2004@gmail.com>
2022-03-14 11:23:20 +0700

    2.1

--- srp.js
+++ srp.js
@@ -1,16 +1,5 @@
-
-$(document).ready(function(){
-$("#el1").draggable().resizable();
-
-$("#sort1").sortable();
-
-$("#el2, #el3").draggable();
-$("#drop1").droppable({accept:"#el2, #el3"},{drop:function(){
-$(this).addClass("ui-state-highlight");
-$(this).html("Элемент был перемещен успешно!");}});
-
-$("#drop2").droppable({accept:"#el3",activeClass:"dragh"},{drop:function(){
-$(this).addClass("ui-state-highlight");
-$(this).html("Элемент был перемещен успешно!");}});
+$(document).ready(function() {
   
+   $("#accordion1").accordion();
+
 });
